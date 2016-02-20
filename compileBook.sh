03528e034fdaf4168f3382547ac9c93e55f179cf
StephaneDucasse <stephane.ducasse@inria.fr>
2016-02-20 09:54:32 +0100

    cleaning more

--- compileBook.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cd book-result
-cp ../BookletMagritte.tex .
-# ln -s -f ../figures . # redundant with the support option in pillar.conf
-texfot latexmk BookletMagritte
